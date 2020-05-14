b29824ccf32ecd4fb38707d26fb8e28d19e83159
Zoinkeren <60384695+Zoinkeren@users.noreply.github.com>
2020-05-14 15:34:05 +0200

    menu udkast1 casper

--- styleGUIDEtrendlog/script.js
+++ styleGUIDEtrendlog/script.js
@@ -5,3 +5,21 @@ const links = document.querySelectorAll('.nav-links li')
 hamburger.addEventListener("click", () =>{
   navLinks.classList.toggle("open");
 });
+
+//checkbox system !om checkbox er blevet valgt eller ej!
+
+function is_checked(){
+  var dataOne = document.getElementById('dataOne').checked;
+  var dataTwo = document.getElementById('dataTwo').checked;
+  var dataThree = document.getElementById('dataThree').checked;
+
+if(dataOne==false && dataTwo==false && dataThree==false)  {
+  alert('please select some data before you go further');
+  return false;
+  }
+
+else {
+  return true;
+    }
+
+}
