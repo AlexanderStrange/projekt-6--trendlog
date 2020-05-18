001b2a4d9bd5565b91eba65495f7e97d2ac4c183
MalleLauritsen <amalie@familienlauritsen.dk>
2020-05-18 18:52:11 +0200

    Change classes to ids
    '.' & '#'

--- assets/javascript/amalie.js
+++ assets/javascript/amalie.js
@@ -1,12 +1,12 @@
 // BURGERMENU
 function burgermenu(){
-	var burgerOne = document.querySelector(".TRVFSS");
+	var burgerOne = document.querySelector("#TRVFSS");
 		if (burgerOne.style.display == "flex") {
 			burgerOne.style.display = "none";
 		} else{
 			burgerOne.style.display = "flex";
 		}
-	var burgerTwo = document.querySelector(".SFAP");
+	var burgerTwo = document.querySelector("#SFAP");
 		if (burgerTwo.style.display == "flex") {
 			burgerTwo.style.display = "none";
 		} else{
