8a030779b1b9d96afa26e2b69d1056108cfad890
MalleLauritsen <amalie@familienlauritsen.dk>
2020-05-19 21:06:29 +0200

    Edit test
    'test1.html & 'amalie.js (test)'

--- testmappen/localstorage/amalie.js
+++ testmappen/localstorage/amalie.js
@@ -1,8 +1,41 @@
 function localStorage(){
-	if (typeof(Storage) !== "undefined"){
-		localStorage.setItem("expandedImg", "dataOne");
-
-		document.querySelector(".containerpreview").innerHTML = localStorage.getItem("dataOne")
-	} else {
-	  document.querySelector(".containerpreview").innerHTML = "Sorry, your browser does not support Web Storage...";
+	if (localStorage.getItem("dataOne") != null) {
+		document.getElementById("expandedImg").src = localStorage.getItem("dataOne");
+	}
+	if (localStorage.getItem("dataTwo") != null) {
+		document.getElementById("expandedImg").src = localStorage.getItem("dataTwo");
+	}
+	if (localStorage.getItem("dataTwo") != null) {
+		document.getElementById("expandedImg").src = localStorage.getItem("dataTree");
+	}
+	if (localStorage.getItem("fundet4") != null) {
+		document.getElementById("expandedImg").src = localStorage.getItem("dataFour");
+	}
 }
+
+/*
+HTML - sættes under hvor item er med value og keyword
+<script type="text/javascript">localStorage.setItem("value", "keyword");</script>
+*/
+// <script type="text/javascript">localStorage.setItem("forkortelse af...", "...det som skal indsættes");</script>
+
+/*
+HTML - nye boks til item ('overbil' - 'expandedImg')
+*/
+
+// PROJEKT 5
+// JavaScript - localStorage
+/*
+function checkImages(){
+	if (localStorage.getItem("fundet1") != null) {
+		document.getElementById("overbil1").src = localStorage.getItem("fundet1");
+	}
+*/
+
+// MULIG KODE
+/*
+function localStorage(){
+	if (localStorage.getItem("dataOne") != null) {
+		document.getElementById("expandedImg").src = localStorage.getItem("dataOne");
+	}
+*/
