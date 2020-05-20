39ba1ddc857c5172d0ff9c3584d1320453221ee0
Karinakragsig <Karina_kragsig@hotmail.com>
2020-05-20 13:26:04 +0200

    Search bar

--- assets/javascript/karina.js
+++ assets/javascript/karina.js
@@ -3,16 +3,25 @@ const search = document.getElementById('search');
 const tip = document.getElementById('tip');
 
 var i =0;
+var message = 'Search';
+var speed = 100;
 
 searchBtn.addEventListener('click',() =>{
-  search.style.width = '80%';
-  search.style.paddingLeft = '60px';
+  search.style.width = '40%';
+  search.style.paddingLeft = '50px';
   search.style.cursor = 'text';
   search.focus();
+
+  search.setAttribute('placeholder', 'Search');
+  typeWriter();
 })
 
 function typeWriter() {
   if (i < message.lenght) {
+    message = search.getAttribute('placeholder') + message.charAt(i);
+    search.setAttribute('placeholder',message);
+    i++;
+    setTimeout(typewriter, speed);
   }
 }
 
