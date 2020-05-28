f97bf97293fa9095bc7fa53d283e3b46d2e8047b
Alexander Strange <alexander.okay@hotmail.com>
2020-05-28 14:50:54 +0200

    Update alexander.js

--- assets/javascript/alexander.js
+++ assets/javascript/alexander.js
@@ -16,9 +16,7 @@ else {
 // script for previewet og miniaturer
 function myFunction(imgs) {
   //get the expanded images
-  let expandImg = document.getElementById("expandedImg");
+  var expandImg = document.getElementById("expandedImg");
   // Use the same src in the expanded image as the image being clicked on from the grid
   expandImg.src = imgs.src;
-  // Show the container element (hidden with CSS)
-  expandImg.parentElement.style.display = "block";
 }
